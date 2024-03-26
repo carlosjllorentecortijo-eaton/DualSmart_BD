b24e05550188e9e80007bbb53fbcc2f7ee1adde5
carlosjllorentecortijo-eaton <carlosjllorentecortijo@eaton.com>
2024-03-26 10:12:15 +0100

    DualSmart_Commands/Inc: add settings specially for custom behavior

---
+++ DualSmart_Commands/Inc/SettingsCustom.h
@@ -0,0 +1,21 @@
+/*
+ * Settings.h
+ *
+ *  Created on: Mar 26, 2024
+ *      Author: E0768084
+ */
+
+#ifndef INC_SETTINGSCUSTOM_H_
+#define INC_SETTINGSCUSTOM_H_
+
+/* Debug related */
+#define DEBUG_LEVEL_NONE			0 /* Disabled */
+#define DEBUG_LEVEL_CRITICAL		1 /* Enabled for critical messages */
+#define DEBUG_LEVEL_WARNING			2 /* Enabled for critical/warning messages */
+#define DEBUG_LEVEL_INFO			3 /* Enabled for critical/warning/info messages */
+#define DEBUG_LEVEL_FULL			4 /* Enabled for all messages */
+
+/* Debug prints */
+#define DEBUG_COMMANDS            DEBUG_LEVEL_FULL
+
+#endif /* INC_SETTINGSCUSTOM_H_ */
